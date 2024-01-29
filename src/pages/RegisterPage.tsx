89c44a01ecfd6fda52498ad2d3a210c51114de12
Daniel An <cruzalexd1@gmail.com>
2024-01-29 10:05:14 -0600

    :art:feat(register): add instanceof error for error message

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -21,79 +21,82 @@ export const RegisterPage = () => {
         try {
             await createUserWithEmailAndPassword(auth, data.email, data.password)
         } catch (error) {
-            setError(error.message.replace('Firebase:', ''))
+            if (error instanceof Error) {
+                setError(error.message.replace('Firebase:', ''))
+            } else {
+                setError('An unknown error occurred.')
+            }
+
+            console.log(data)
         }
 
-        console.log(data)
-    }
+        return (
+            <>
+                <div className="container d-flex justify-content-center">
+                    <div className="card mt-5 col-4">
+                        <div className="card-body align-item-center">
+                            <h5 className="card-title">Register please</h5>
+                            <p className="card-title mb-5"> Sign in to continue</p>
+                            <form onSubmit={handleSubmit(createUser)}>
+                                <label>Email or Phone number</label>
+                                <div className="mt-1 mb-3">
+                                    <input type="text"
+                                        {...register('email', { required: 'Email is required', pattern: emailValidation })}
+                                        className="form-control"
+                                        placeholder="Please type your email"
+                                    />
+                                    {
+                                        errors.email && <span className="text-danger">{errors.email.message}</span>
+                                    }
+                                </div>
+                                <div className="mt-1">
+                                    <label>Password</label>
+                                    <input type="password"
+                                        {...register('password', { required: 'Password is required', minLength: minPassword, maxLength: maxPassword })}
+                                        className="form-control"
+                                        placeholder="Please type your password"
+                                    />
+                                    {
+                                        errors.password && <span className="text-danger">{errors.password.message}</span>
+                                    }
+                                </div>
+                                <div className="mt-1 mb-5">
+                                    <label>Confirm your Password</label>
+                                    <input type="password"
+                                        {...register('confirmPassword', { required: 'Confirmation password is required', minLength: minPassword, maxLength: maxPassword })}
+                                        className="form-control"
+                                        placeholder="Please type your confirmation password"
+                                    />
+                                    {
+                                        errors.password && <span className="text-danger">{errors.password.message}</span>
+                                    }
+                                </div>
+                                <div className="mt-3">
+                                    <label htmlFor="profile-name">What is your name?</label>
+                                    <input type="profile-name"
+                                        name="text"
+                                        className="form-control"
+                                        placeholder="Please type your profile name: "
+                                    />
+                                </div>
 
-    return (
-        <>
-            <div className="container d-flex justify-content-center">
-                <div className="card mt-5 col-4">
-                    <div className="card-body align-item-center">
-                        <h5 className="card-title">Register please</h5>
-                        <p className="card-title mb-5"> Sign in to continue</p>
-                        <form onSubmit={handleSubmit(createUser)}>
-                            <label>Email or Phone number</label>
-                            <div className="mt-1 mb-3">
-                                <input type="text"
-                                    {...register('email', { required: 'Email is required', pattern: emailValidation })}
-                                    className="form-control"
-                                    placeholder="Please type your email"
-                                />
-                                {
-                                    errors.email && <span className="text-danger">{errors.email.message}</span>
-                                }
-                            </div>
-                            <div className="mt-1">
-                                <label>Password</label>
-                                <input type="password"
-                                    {...register('password', { required: 'Password is required', minLength: minPassword, maxLength: maxPassword })}
-                                    className="form-control"
-                                    placeholder="Please type your password"
-                                />
-                                {
-                                    errors.password && <span className="text-danger">{errors.password.message}</span>
-                                }
-                            </div>
-                            <div className="mt-1 mb-5">
-                                <label>Confirm your Password</label>
-                                <input type="password"
-                                    {...register('confirmPassword', { required: 'Confirmation password is required', minLength: minPassword, maxLength: maxPassword })}
-                                    className="form-control"
-                                    placeholder="Please type your confirmation password"
-                                />
-                                {
-                                    errors.password && <span className="text-danger">{errors.password.message}</span>
-                                }
-                            </div>
-                            <div className="mt-3">
-                                <label htmlFor="profile-name">What is your name?</label>
-                                <input type="profile-name"
-                                    name="text"
-                                    className="form-control"
-                                    placeholder="Please type your profile name: "
-                                />
-                            </div>
+                                <div className=" d-flex justify-content-center mt-4">
+                                    <button type="submit" className="btn btn-danger w-50">Create account</button>
+                                </div>
+                            </form>
+                            {
+                                error && <span className="text-danger">{error && `${error} Login instead`}</span>
+                            }
+                        </div>
 
-                            <div className=" d-flex justify-content-center mt-4">
-                                <button type="submit" className="btn btn-danger w-50">Create account</button>
-                            </div>
-                        </form>
-                        {
-                            error && <span className="text-danger">{error && `${error} Login instead`}</span>
-                        }
+                        <div className="d-flex justify-content-between mt-4">
+                            <Link className="register-link m-2" to={'/'}> Home </Link>
+                            <Link className="register-link m-2" to={'/login'}> Login instead </Link>
+
+                        </div>
+
                     </div>
-
-                    <div className="d-flex justify-content-between mt-4">
-                        <Link className="register-link m-2" to={'/'}> Home </Link>
-                        <Link className="register-link m-2" to={'/login'}> Login instead </Link>
-
-                    </div>
-
                 </div>
-            </div>
-        </>
-    )
-}
+            </>
+        )
+    }
