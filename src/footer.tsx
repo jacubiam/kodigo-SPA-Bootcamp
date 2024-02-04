c9fd21e438c9d75793d680bb810ae063c9e4b4ff
Alejandro Ortiz <124010151+AlePhantom01@users.noreply.github.com>
2024-02-04 12:34:08 -0600

    Update footer.tsx

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,69 +1,123 @@
-<footer class="bg-light text-center text-white">
-  <!-- Grid container -->
-  <div class="container p-4 pb-0">
-    <!-- Section: Social media -->
-    <section class="mb-4">
-      <!-- Facebook -->
-      <a
-        class="btn text-white btn-floating m-1"
-        style="background-color: #3b5998;"
-        href="#!"
-        role="button"
-        ><i class="fab fa-facebook-f"></i
-      ></a>
+<div class="container my-5">
+ 
+  <footer
+          className="text-center text-lg-start text-white"
+          style={{"background-color: #929fba"}}
+          >
+    
+    <div class="container p-4 pb-0">
+     
+      <section className="">
+     
+        <div className="row">
+         
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 className="text-uppercase mb-4 font-weight-bold">
+              Kodigo Academy
+            </h6>
+            <p>
+             Aprendiendo para el futuro.
+            </p>
+          </div>
+          
 
-      <!-- Twitter -->
-      <a
-        class="btn text-white btn-floating m-1"
-        style="background-color: #55acee;"
-        href="#!"
-        role="button"
-        ><i class="fab fa-twitter"></i
-      ></a>
+          <hr className="w-100 clearfix d-md-none" />
 
-      <!-- Google -->
-      <a
-        class="btn text-white btn-floating m-1"
-        style="background-color: #dd4b39;"
-        href="#!"
-        role="button"
-        ><i class="fab fa-google"></i
-      ></a>
+          
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 className="text-uppercase mb-4 font-weight-bold">Productos y Servicios</h6>
+            <p>
+              <a className="text-white">Kodigo Academy</a>
+            </p>
+           
+          </div>
+                    <hr className="w-100 clearfix d-md-none" />
 
-      <!-- Instagram -->
-      <a
-        class="btn text-white btn-floating m-1"
-        style="background-color: #ac2bac;"
-        href="#!"
-        role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
+          
+          <hr className="w-100 clearfix d-md-none" />
 
-      <!-- Linkedin -->
-      <a
-        class="btn text-white btn-floating m-1"
-        style="background-color: #0082ca;"
-        href="#!"
-        role="button"
-        ><i class="fab fa-linkedin-in"></i
-      ></a>
-      <!-- Github -->
-      <a
-        class="btn text-white btn-floating m-1"
-        style="background-color: #333333;"
-        href="#!"
-        role="button"
-        ><i class="fab fa-github"></i
-      ></a>
-    </section>
-    <!-- Section: Social media -->
-  </div>
-  <!-- Grid container -->
+          
+          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 className="text-uppercase mb-4 font-weight-bold">Contactanos</h6>
+            <p><i className="fas fa-home mr-3"></i> San Salvador, El Salvador, Centro América</p>
+            <p><i className="fas fa-envelope mr-3"></i> info@Kodigo.com</p>
+            <p><i className="fas fa-phone mr-3"></i> + 503 XXXXXXXXX</p>
+            <p><i className="fas fa-print mr-3"></i> + 503 XXXXXXXXX</p>
+          </div>
+          
 
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2024 Kodigo Academy:
-    <a class="text-white" href="https://mdbootstrap.com/">KODIGO</a>
-  </div>
-  <!-- Copyright -->
-</footer>
+        
+          <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 className="text-uppercase mb-4 font-weight-bold">Siguenos en nuestras redes sociales</h6>
+
+                      <a
+               className="btn btn-primary btn-floating m-1"
+               style={{"background-color: #3b5998"}}
+               href="#!"
+               role="button"
+               ><i className="fab fa-facebook-f"></i
+              ></a>
+
+           
+            <a
+               className="btn btn-primary btn-floating m-1"
+               style={{"background-color: #55acee"}}
+               href="#!"
+               role="button"
+               ><i className="fab fa-twitter"></i
+              ></a>
+
+           
+            <a
+               className="btn btn-primary btn-floating m-1"
+               style={{"background-color: #dd4b39"}}
+               href="#!"
+               role="button"
+               ><i className="fab fa-google"></i
+              ></a>
+
+           
+            <a
+               className="btn btn-primary btn-floating m-1"
+               style={{"background-color: #ac2bac"}}
+               href="#!"
+               role="button"
+               ><i className="fab fa-instagram"></i
+              ></a>
+
+           
+            <a
+               className="btn btn-primary btn-floating m-1"
+               style={{"background-color: #0082ca"}}
+               href="#!"
+               role="button"
+               ><i className="fab fa-linkedin-in"></i
+              ></a>
+           
+            <a
+               className="btn btn-primary btn-floating m-1"
+               style={{"background-color: #333333"}}
+               href="#!"
+               role="button"
+               ><i className="fab fa-github"></i
+              ></a>
+          </div>
+        </div>
+       
+      </section>
+    
+    </div>
+  
+    <div
+         className="text-center p-3"
+         style={{"background-color: rgba(0, 0, 0, 0.2)"}}
+         >
+      © 2020 Copyright: Kodigo Academy
+      <a className="text-white" href=" "
+         >MDBootstrap.com</a
+        >
+    </div>
+
+  </footer>
+ 
+</div>
