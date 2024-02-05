10c3988f8a9b020f8bdb368704ba5e9a6bdce5ce
erbiti <tommy.ponce@gmail.com>
2024-02-05 12:36:18 -0600

    :feat:(header): Add header

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,8 +4,12 @@ import { useEffect, useState } from "react"
 import BootcampCard from "../components/HomePage/BootcampCard"
 import { Link } from "react-router-dom"
 import BootcampsDocentes from "../components/HomePage/BootcampsDocentes"
-import Carousel from "../components/HomePage/Carousel"
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import logo from "../assets/logo-kodigo.png";
 
 const HomePage = () => {
     const [bootcamps, setBootcamps] = useState<DocumentData[]>([])
@@ -27,14 +31,45 @@ const HomePage = () => {
 
     return (
         <>
-            <header className="text-center" style={{ height: "100px" }}>
-                <h3>Placeholder del header</h3>
-                <Link className="me-3" to={'/signup'}>Crear cuenta</Link>
-                <Link to={'/login'}>Iniciar sesión</Link>
+            <header className=" header-home text-center" style={{ height: "100px" }}>
+                <Navbar expand="lg" className="">
+                    <Container>
+                        <Navbar className="">
+                            <Navbar.Brand href="#home">
+                                <img
+                                    src={logo}
+                                    // width="180"
+                                    height="40"
+                                    className="d-inline-block align-top"
+                                    alt="React Bootstrap logo"
+                                />
+                            </Navbar.Brand>
+                        </Navbar>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" >
+                            <Nav className="">
+                                <NavDropdown title="BootCamps" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Link </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Link </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Link </NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link href="#home" className="px-3">CPD</Nav.Link>
+                                <Nav.Link href="#link" className="px-3">Alliances</Nav.Link>
+                                <Nav.Link href="#link" className="px-3">About Us</Nav.Link>
+                                <Nav.Link href="#link" className="px-3">Blog</Nav.Link>
+                                <Nav.Link href="#link" className="px-3">FAQ</Nav.Link>
+                            </Nav>
+                            {/* <h3>Placeholder del header</h3> */}
+                            <div className="btn-sign-in-up" >
+                                <Link className="me-3" to={'/signup'}><Button variant="success"className="bg-white text-dark sign-in">Crear cuenta</Button>{' '}</Link>
+                                {/* <Link to={'/signup'}><Button variant="default">Crear cuenta</Button>{' '}</Link> */}
+                                <Link to={'/login'}><Button variant="secondary sign-up"className="ms-3">Iniciar sesión</Button>{' '}</Link>
+                            </div></Navbar.Collapse>
+                    </Container>
+                </Navbar>
+
             </header>
-            <main>
-            <Carousel/>
-            <section className="container" >
+            <div className="container" >
                 <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
                     {bootcamps && bootcamps.map((bootcamp) => {
                         return (
@@ -44,9 +79,9 @@ const HomePage = () => {
                         )
                     })}
                 </div>
-            </section>
-            <BootcampsDocentes/>
-            </main>
+            </div>
+            <BootcampsDocentes />
+
         </>
     )
 }
