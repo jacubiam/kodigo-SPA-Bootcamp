7a71bcb44d845f04b78848ec42deffe86c16406c
Joaquin Juarez <joaquinjuarez17@hotmail.com>
2024-02-05 21:56:03 -0600

    :recycle:refactor(home):move heeader and banner to a separete component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,11 @@
+import { useEffect, useState } from "react"
 import { collection, getDocs, DocumentData, orderBy, query } from "firebase/firestore"
 import { db } from "../firebase"
-import { useEffect, useState } from "react"
 import BootcampCard from "../components/HomePage/BootcampCard"
-import { Link } from "react-router-dom"
 import BootcampsDocentes from "../components/HomePage/BootcampsDocentes"
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import Button from 'react-bootstrap/Button'
-import logo from "../assets/logo-kodigo.png"
 import Carousel from "../components/HomePage/Carousel"
 import Banner from "../components/HomePage/Banner"
+import Header from "../components/Commons/Header"
 
 const HomePage = () => {
     const [bootcamps, setBootcamps] = useState<DocumentData[]>([])
@@ -34,42 +28,7 @@ const HomePage = () => {
     return (
         <>
             <header className=" header-home text-center" style={{ height: "100px" }}>
-                <Navbar expand="lg" className="">
-                    <Container>
-                        <Navbar className="">
-                            <Navbar.Brand href="#home">
-                                <img
-                                    src={logo}
-                                    // width="180"
-                                    height="40"
-                                    className="d-inline-block align-top"
-                                    alt="React Bootstrap logo"
-                                />
-                            </Navbar.Brand>
-                        </Navbar>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" >
-                            <Nav className="">
-                                <NavDropdown title="BootCamps" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Link </NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Link </NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Link </NavDropdown.Item>
-                                </NavDropdown>
-                                <Nav.Link href="#home" className="px-3">CPD</Nav.Link>
-                                <Nav.Link href="#link" className="px-3">Alliances</Nav.Link>
-                                <Nav.Link href="#link" className="px-3">About Us</Nav.Link>
-                                <Nav.Link href="#link" className="px-3">Blog</Nav.Link>
-                                <Nav.Link href="#link" className="px-3">FAQ</Nav.Link>
-                            </Nav>
-                            {/* <h3>Placeholder del header</h3> */}
-                            <div className="btn-sign-in-up" >
-                                <Link className="me-3" to={'/signup'}><Button variant="success" className="bg-white text-dark sign-in">Crear cuenta</Button>{' '}</Link>
-                                {/* <Link to={'/signup'}><Button variant="default">Crear cuenta</Button>{' '}</Link> */}
-                                <Link to={'/login'}><Button variant="secondary sign-up" className="ms-3">Iniciar sesión</Button>{' '}</Link>
-                            </div></Navbar.Collapse>
-                    </Container>
-                </Navbar>
-
+                <Header />
             </header>
             <main>
                 <Banner />
